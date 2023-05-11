e2376703b0307ffc929286838623022fc147e8bc
Paulo <paulo.evangelista@sou.inteli.edu.br>
2023-05-11 17:59:32 -0300

    feat(): improved homepage

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,6 +1,6 @@
 import close from '../../assets/x.svg'
 import Image from 'next/image'
-import { motion } from 'framer-motion'
+import { motion, AnimatePresence } from 'framer-motion'
 
 interface MenuProps {
   setMenuState: () => void
@@ -8,23 +8,27 @@ interface MenuProps {
 
 const Menu: React.FC<MenuProps> = ({ setMenuState }) => {
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      className=" h-full bg-mainPink z-10 pt-30 w-full text-center absolute transition-all"
-    >
-      <div
-        className="w-full justify-start flex ml-2 mt-2"
-        onClick={setMenuState}
+    <AnimatePresence>
+      <motion.div
+        key="menu"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        className=" h-full bg-mainPink z-10 pt-30 w-full text-center absolute transition-all"
       >
-        <Image src={close}></Image>
-      </div>
-      <h1>HELLOOOOO</h1>
-      <h1>HELLOOOOO</h1>
-      <h1>HELLOOOOO</h1>
-      <h1>HELLOOOOO</h1>
-      <h1>HELLOOOOO</h1>
-    </motion.div>
+        <div
+          className="w-full justify-start flex ml-2 mt-2"
+          onClick={setMenuState}
+        >
+          <Image src={close}></Image>
+        </div>
+        <h1>HELLOOOOO</h1>
+        <h1>HELLOOOOO</h1>
+        <h1>HELLOOOOO</h1>
+        <h1>HELLOOOOO</h1>
+        <h1>HELLOOOOO</h1>
+      </motion.div>
+    </AnimatePresence>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,31 @@
-import Header from '../components/header'
-import ExtLink from '../components/ext-link'
-import Features from '../components/features'
-import sharedStyles from '../styles/shared.module.css'
-import { motion } from 'framer-motion'
+import React, { useRef, useState } from 'react'
+import { motion, Variants } from 'framer-motion'
 import Image from 'next/image'
+import pattern from '../../assets/pattern.svg'
 import perfil from '../../assets/perfil.png'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import { Pagination } from 'swiper'
+
+const textVariants: Variants = {
+  hidden: {
+    scale: 0.8,
+    opacity: 0,
+    translateY: 50,
+    translateX: 0,
+  },
+  visible: {
+    scale: 1,
+    opacity: 1,
+    translateY: 0,
+    translateX: 0,
+    transition: {
+      delay: 0.5,
+      duration: 0.5,
+    },
+  },
+}
 
 export default function Index() {
   return (
@@ -13,28 +34,7 @@ export default function Index() {
         <div className="absolute top-52">
           <Image height={460} src={perfil}></Image>
         </div>
-        <motion.div
-          initial="hidden"
-          animate="visible"
-          variants={{
-            hidden: {
-              scale: 0.8,
-              opacity: 0,
-              translateY: 50,
-              translateX: 0,
-            },
-            visible: {
-              scale: 1,
-              opacity: 1,
-              translateY: 0,
-              translateX: 0,
-              transition: {
-                delay: 0.5,
-                duration: 0.5,
-              },
-            },
-          }}
-        >
+        <motion.div initial="hidden" animate="visible" variants={textVariants}>
           <div className="mt-14">
             <p className="text-white text-7xl font-extralight">Viva o</p>
           </div>
@@ -68,8 +68,27 @@ export default function Index() {
           </div>
         </motion.div>
       </div>
-      <div className="bg-white h-96">
-        <p>next container</p>
+      <Swiper
+        loop={true}
+        slidesPerView={3}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        className="mySwiper w-full h-12"
+      >
+        <SwiperSlide className="h-12">Slide 1</SwiperSlide>
+        <SwiperSlide className="h-12">Slide 2</SwiperSlide>
+        <SwiperSlide className="h-12">Slide 3</SwiperSlide>
+        <SwiperSlide className="h-12">Slide 4</SwiperSlide>
+        <SwiperSlide className="h-12">Slide 5</SwiperSlide>
+        <SwiperSlide className="h-12">Slide 6</SwiperSlide>
+        <SwiperSlide className="h-12">Slide 7</SwiperSlide>
+        <SwiperSlide className="h-12">Slide 8</SwiperSlide>
+        <SwiperSlide className="h-12">Slide 9</SwiperSlide>
+      </Swiper>
+      <div className=" bg-white h-[700px]">
+        <Image src={pattern}></Image>
       </div>
     </>
   )

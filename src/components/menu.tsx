00e7ca5bd04e89db644285198441c4d3d73913c8
Paulo <paulo.evangelista@sou.inteli.edu.br>
2023-05-11 01:21:19 -0300

    feat(): added framer-motion

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,5 +1,6 @@
 import close from '../../assets/x.svg'
 import Image from 'next/image'
+import { motion } from 'framer-motion'
 
 interface MenuProps {
   setMenuState: () => void
@@ -7,7 +8,11 @@ interface MenuProps {
 
 const Menu: React.FC<MenuProps> = ({ setMenuState }) => {
   return (
-    <div className=" h-full bg-mainPink z-10 pt-30 w-full text-center absolute transition-all">
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      className=" h-full bg-mainPink z-10 pt-30 w-full text-center absolute transition-all"
+    >
       <div
         className="w-full justify-start flex ml-2 mt-2"
         onClick={setMenuState}
@@ -19,7 +24,7 @@ const Menu: React.FC<MenuProps> = ({ setMenuState }) => {
       <h1>HELLOOOOO</h1>
       <h1>HELLOOOOO</h1>
       <h1>HELLOOOOO</h1>
-    </div>
+    </motion.div>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Header from '../components/header'
 import ExtLink from '../components/ext-link'
 import Features from '../components/features'
 import sharedStyles from '../styles/shared.module.css'
+import { motion } from 'framer-motion'
 import Image from 'next/image'
 import perfil from '../../assets/perfil.png'
 
@@ -12,38 +13,58 @@ export default function Index() {
         <div className="absolute top-52">
           <Image height={460} src={perfil}></Image>
         </div>
-
-        <div className="mt-14">
-          <p className="text-white text-7xl font-extralight">Viva o</p>
-        </div>
-        <div className="w-full flex justify-end">
-          <p className="text-white text-6xl xs:text-7xl font-bold mr-4">
-            MELHOR
-          </p>
-        </div>
-        <div className="w-full flex justify-end">
-          <p className="text-white text-4xl xs:text-6xl font-thin mr-4">
-            da <span className="font-bold text-5xl xs:text-6xl">VIDA</span>
-          </p>
-        </div>
-        <div className=" flex justify-end">
-          <p className="text-white text-xs xs:text-sm font-extralight mr-4">
-            Medicina preventiva, estilo de vida
-          </p>
-        </div>
-        <div className=" flex justify-end">
-          <p className="text-white text-xs xs:text-sm font-extralight mr-4">
-            e tratamento pós menopausa
-          </p>
-        </div>
-        <div className="flex justify-end">
-          <div className="w-1/3 text-center mr-8 mt-4">
-            <p className="text-sm text-white font-extralight">
-              A doutora que te deixa <span className="font-bold">longe</span>{' '}
-              dos <span className="font-bold">médicos</span>
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          variants={{
+            hidden: {
+              scale: 0.8,
+              opacity: 0,
+              translateX: 100,
+            },
+            visible: {
+              scale: 1,
+              opacity: 1,
+              translateX: 0,
+              transition: {
+                delay: 0.5,
+                duration: 0.5,
+              },
+            },
+          }}
+        >
+          <div className="mt-14">
+            <p className="text-white text-7xl font-extralight">Viva o</p>
+          </div>
+          <div className="w-full flex justify-end">
+            <p className="text-white text-6xl xs:text-7xl font-bold mr-4">
+              MELHOR
             </p>
           </div>
-        </div>
+          <div className="w-full flex justify-end">
+            <p className="text-white text-4xl xs:text-6xl font-thin mr-4">
+              da <span className="font-bold text-5xl xs:text-6xl">VIDA</span>
+            </p>
+          </div>
+          <div className=" flex justify-end">
+            <p className="text-white text-xs xs:text-sm font-extralight mr-4">
+              Medicina preventiva, estilo de vida
+            </p>
+          </div>
+          <div className=" flex justify-end">
+            <p className="text-white text-xs xs:text-sm font-extralight mr-4">
+              e tratamento pós menopausa
+            </p>
+          </div>
+          <div className="flex justify-end">
+            <div className="w-1/3 text-center mr-8 mt-4">
+              <p className="text-sm text-white font-extralight">
+                A doutora que te deixa <span className="font-bold">longe</span>{' '}
+                dos <span className="font-bold">médicos</span>
+              </p>
+            </div>
+          </div>
+        </motion.div>
       </div>
       <div className="bg-white h-96">
         <p>next container</p>

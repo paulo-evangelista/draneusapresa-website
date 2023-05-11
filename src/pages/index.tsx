f603e8fff268edb1c63e2205aa4cf065756689a0
Paulo <paulo.evangelista@sou.inteli.edu.br>
2023-05-11 01:26:53 -0300

    fix(): framer bug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,11 +20,13 @@ export default function Index() {
             hidden: {
               scale: 0.8,
               opacity: 0,
-              translateX: 100,
+              translateY: 50,
+              translateX: 0,
             },
             visible: {
               scale: 1,
               opacity: 1,
+              translateY: 0,
               translateX: 0,
               transition: {
                 delay: 0.5,

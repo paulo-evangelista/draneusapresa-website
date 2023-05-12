cb91d142422383d70333a379e50494d84b178c64
Paulo <paulo.evangelista@sou.inteli.edu.br>
2023-05-11 22:39:23 -0300

    feat(): better animations

---
+++ src/animations/index.ts
@@ -0,0 +1,66 @@
+import { Variants } from 'framer-motion'
+
+export const titleVariant1: Variants = {
+  hidden: {
+    scale: 0.9,
+    opacity: 0,
+    translateY: 30,
+    translateX: 0,
+  },
+  visible: {
+    scale: 1,
+    opacity: 1,
+    translateY: 0,
+    translateX: 0,
+    transition: {
+      delay: 1,
+      duration: 0.5,
+    },
+  },
+}
+
+export const titleVariant2: Variants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      delay: 1.5,
+      duration: 0.6,
+    },
+  },
+}
+
+export const titleVariant3: Variants = {
+  hidden: {
+    scale: 0.9,
+    opacity: 0,
+    translateY: 30,
+    translateX: 0,
+  },
+  visible: {
+    scale: 1,
+    opacity: 1,
+    translateY: 0,
+    translateX: 0,
+    transition: {
+      ease: 'circOut',
+      delay: 2,
+      duration: 0.5,
+    },
+  },
+}
+
+export const titleVariant4: Variants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      delay: 5,
+      duration: 1.2,
+    },
+  },
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,47 +9,40 @@ import 'swiper/css'
 import 'swiper/css/autoplay'
 import CarouselCard from '../components/carouselCard'
 import Link from 'next/link'
-
-const textVariants: Variants = {
-  hidden: {
-    scale: 0.8,
-    opacity: 0,
-    translateY: 50,
-    translateX: 0,
-  },
-  visible: {
-    scale: 1,
-    opacity: 1,
-    translateY: 0,
-    translateX: 0,
-    transition: {
-      delay: 0.5,
-      duration: 0.5,
-    },
-  },
-}
+import {
+  titleVariant1,
+  titleVariant2,
+  titleVariant3,
+  titleVariant4,
+} from '../animations'
 
 export default function Index() {
   return (
     <>
       <div className="h-[572px]">
-        <div className="absolute top-52">
+        <div className="absolute z-10 top-52">
           <Image height={460} src={perfil}></Image>
         </div>
-        <motion.div initial="hidden" animate="visible" variants={textVariants}>
+        <motion.div initial="hidden" animate="visible" variants={titleVariant1}>
           <div className="mt-14">
             <p className="text-white text-7xl font-extralight">Viva o</p>
           </div>
+        </motion.div>
+        <motion.div initial="hidden" animate="visible" variants={titleVariant2}>
           <div className="w-full flex justify-end">
             <p className="text-white text-6xl xs:text-7xl font-bold mr-4">
               MELHOR
             </p>
           </div>
+        </motion.div>
+        <motion.div initial="hidden" animate="visible" variants={titleVariant3}>
           <div className="w-full flex justify-end">
             <p className="text-white text-4xl xs:text-6xl font-thin mr-4">
               da <span className="font-bold text-5xl xs:text-6xl">VIDA</span>
             </p>
           </div>
+        </motion.div>
+        <motion.div initial="hidden" animate="visible" variants={titleVariant4}>
           <div className=" flex justify-end">
             <p className="text-white text-xs xs:text-sm font-extralight mr-4">
               Medicina preventiva, estilo de vida
@@ -75,7 +68,7 @@ export default function Index() {
           <motion.div
             initial="hidden"
             animate="visible"
-            variants={textVariants}
+            variants={titleVariant1}
           >
             <Swiper
               modules={[Autoplay]}
@@ -124,7 +117,7 @@ export default function Index() {
           </motion.div>
           <div className="text-center mt-8">
             <p className="text-2xl text-black">Quer saber mais?</p>
-            <p className="text-3xl text-black">Acesse meu blog!</p>
+
             <Link href={'/blog'}>
               <button className="bg-pink-400 text-white rounded-lg p-3 text-2xl mb-10">
                 Acessar o blog

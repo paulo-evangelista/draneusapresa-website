385e7d4fc134614578658c8240bfa98848040d24
Paulo <paulo.evangelista@sou.inteli.edu.br>
2023-07-30 00:49:54 -0300

    fix: ✨ fixed cursor on links

--- src/components/topbar/desktop/DesktopServicesDropdown.tsx
+++ src/components/topbar/desktop/DesktopServicesDropdown.tsx
@@ -9,29 +9,29 @@ const DesktopServicesDropdown = () => {
       className="absolute grid grid-cols-2 top-14 right-[31%] rounded-lg bg-white shadow-lg"
     >
       <div className="p-4 pb-0 text-center">
-        <p className="pt-2 text-lg text-mainPink hover:scale-110 transition">
+        <p className="pt-2 hover:cursor-pointer text-lg text-mainPink hover:scale-110 transition">
           Medicina Preventiva
         </p>
         <hr className='border border-pink-300 w-16 mx-auto rounded mt-1 mb-4'/>
-        <p className="pt-2 text-lg text-mainPink hover:scale-110 transition">
+        <p className="pt-2 hover:cursor-pointer text-lg text-mainPink hover:scale-110 transition">
           Climatério e Menopausa
         </p>
         <hr className='border border-pink-300 w-16 mx-auto rounded mt-1 mb-4'/>
-        <p className="pt-2 text-lg text-mainPink hover:scale-110 transition">
+        <p className="pt-2 hover:cursor-pointer text-lg text-mainPink hover:scale-110 transition">
           Cirurgia Ginecológica
         </p>
         <hr className='border border-pink-300 w-16 mx-auto rounded mt-1 mb-4'/>
       </div>
       <div className="p-4 text-center">
-        <p className="pt-2 text-lg text-mainPink hover:scale-110 transition">
+        <p className="pt-2 hover:cursor-pointer text-lg text-mainPink hover:scale-110 transition">
           Ginecologia Clínica
         </p>
         <hr className='border border-pink-300 w-16 mx-auto rounded mt-1 mb-4'/>
-        <p className="pt-2 text-lg text-mainPink hover:scale-110 transition">
+        <p className="pt-2 hover:cursor-pointer text-lg text-mainPink hover:scale-110 transition">
           Longevidade Saudável
         </p>
         <hr className='border border-pink-300 w-16 mx-auto rounded mt-1 mb-4'/>
-        <p className="pt-2 text-lg text-mainPink hover:scale-110 transition">
+        <p className="pt-2 hover:cursor-pointer text-lg text-mainPink hover:scale-110 transition">
           Laser Ginecológico
         </p>
         <hr className='border border-pink-300 w-16 mx-auto rounded mt-1 mb-2'/>

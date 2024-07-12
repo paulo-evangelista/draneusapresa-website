e5ee88871f03bbf82095f1a2f605452af4331f6f
paulo-evangelista <paulo.evangelista@sou.inteli.edu.br>
2024-07-12 03:00:30 -0300

    feat: ✨ started select input

--- src/app/agende-online/page.tsx
+++ src/app/agende-online/page.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import { Select } from '@/components/ui/select'
+import { SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from '@radix-ui/react-select'
 
 export default function AgendeOnline() {
   return (
@@ -17,13 +19,27 @@ export default function AgendeOnline() {
           <Input id="name" placeholder='Digite aqui'/>
         </div>
         <div className="px-4 pt-4">
-          <Label htmlFor="name">Seu telefone com DDD</Label>
-          <Input id="name" placeholder='(44) 912345678'/>
+          <Label htmlFor="phone">Seu telefone com DDD</Label>
+          <Input id="phone" placeholder='(44) 912345678'/>
           <p className='text-sm text-neutral-300'>Vamos entrar em contato pelo WhatsApp</p>
         </div>
         <div className="px-4 pt-2">
-          <Label htmlFor="name">Qual seu nome?</Label>
-          <Input id="name" placeholder='Digite aqui'/>
+          <Label htmlFor="options">Qual seu nome?</Label>
+          <Select>
+          <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Select a fruit" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel>Fruits</SelectLabel>
+          <SelectItem value="apple">Apple</SelectItem>
+          <SelectItem value="banana">Banana</SelectItem>
+          <SelectItem value="blueberry">Blueberry</SelectItem>
+          <SelectItem value="grapes">Grapes</SelectItem>
+          <SelectItem value="pineapple">Pineapple</SelectItem>
+        </SelectGroup>
+      </SelectContent>
+          </Select>
         </div>
       </div>
     </div>

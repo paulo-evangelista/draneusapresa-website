cfdfdc5220157b63021fabe75896eb1d22c5b67d
Paulo Evangelista <Paulo Evangelista>
2024-07-19 02:38:45 -0300

    .

--- src/app/agende-online/page.tsx
+++ src/app/agende-online/page.tsx
@@ -11,10 +11,12 @@ import { Select,
   SelectTrigger,
   SelectValue,
  } from '@/components/ui/select'
-
+import { Textarea } from '@/components/ui/textarea'
 import { Calendar } from '@/components/ui/calendar'
 import { ptBR } from 'date-fns/locale'
-
+import { Button } from '@/components/ui/button'
+import whatsAppLogo from "assets/whatsapp.svg"
+import Image from 'next/image'
 
 function formatarTelefone(v: String) {
 
@@ -38,20 +40,22 @@ export default function AgendeOnline() {
   const [phone, setPhone] = React.useState('')
 
   return (
-    <div className="bg-white min-h-screen items-center text-center">
+    <div className="bg-white max-w-md mx-auto items-center text-center">
       <div className="pt-1">
         <h1 className="text-3xl px-10 mb-2 text-black">
           Conquiste uma vida melhor{' '}
           <span className="text-mainPink">sem sair de casa! </span>
         </h1>
       </div>
-      <div className="rounded-xl shadow-2xl text-left text-white bg-mainPink mx-auto">
+      <div className="rounded-t-xl shadow-2xl text-left text-white bg-mainPink mx-auto">
         <div className="px-4 pt-4">
           <Label htmlFor="name">Qual seu nome?</Label>
-          <Input id="name" className='text-black' placeholder="Digite aqui" />
+          <Input id="name" className='text-black w-80' placeholder="Digite aqui" />
         </div>
-        <div className="px-4 pt-4">
+        <div className="px-4 pt-2">
           <Label htmlFor="phone">Seu telefone com DDD</Label>
+          <div className='flex bg-neutral-100 w-64 rounded-lg pl-1'>
+            <Image src={whatsAppLogo} alt="whatsapp logo" height={32}></Image>
           <Input
             id="phone"
             inputMode='numeric'
@@ -59,15 +63,23 @@ export default function AgendeOnline() {
             placeholder="(__) _____-____"
             value={formatarTelefone(phone)}
             onChange={(e) => setPhone(e.target.value)}
-          />
+            />
+            </div>
         </div>
         <div className="px-4 pt-2 text-black">
           <Label htmlFor="options" className='text-white'>No que podemos te ajudar?</Label>
           <Select >
-            <SelectTrigger className="w-[180px]">
+            <SelectTrigger className="w-[320px]">
               <SelectValue className='text-neutral-400' placeholder="Escolha aqui" />
             </SelectTrigger>
-            <SelectContent>
+            <SelectContent
+            					ref={(ref) => {
+                        if (!ref) return;
+                        ref.ontouchstart = (e) => {
+                          e.preventDefault();
+                        };
+                      }}
+            >
               <SelectGroup>
                 <SelectItem value="apple">Apple</SelectItem>
                 <SelectItem value="banana">Banana</SelectItem>
@@ -78,9 +90,9 @@ export default function AgendeOnline() {
             </SelectContent>
           </Select>
         </div>
-        <div className='pt-4 text-center'>
+        <div className='pt-8 text-center'>
 
-    <p>Escolha um dia para sua consulta</p>
+    <p>Escolha seu dia preferido para a consulta</p>
       <div className='bg-white mt-1 rounded-lg shadow-xl w-min mx-auto'>
       <Calendar
       id='calendar'
@@ -108,16 +120,32 @@ export default function AgendeOnline() {
             <SelectTrigger className="w-[200px]">
               <SelectValue className='text-neutral-400' placeholder="Manhã" />
             </SelectTrigger>
-            <SelectContent>
+            <SelectContent
+            					ref={(ref) => {
+                        if (!ref) return;
+                        ref.ontouchstart = (e) => {
+                          e.preventDefault();
+                        };
+                      }}
+            >
               <SelectGroup>
-                <SelectItem value="manha" onClick={(e) => e.stopPropagation()}>Manhã</SelectItem>
-                <SelectItem value="inicio tarde"onClick={(e) => e.stopPropagation()}>Início da tarde</SelectItem>
-                <SelectItem value="fim tarde"onClick={(e) => e.stopPropagation()}>Fim da tarde</SelectItem>
+                <SelectItem value="manha">Manhã</SelectItem>
+                <SelectItem value="inicio tarde">Início da tarde</SelectItem>
+                <SelectItem value="fim tarde">Fim da tarde</SelectItem>
               </SelectGroup>
             </SelectContent>
           </Select>
         </div>
+        <div className="px-4 pt-4">
+          <Label htmlFor="aditional">Alguma informação adicional? (Opcional)</Label>
+          <Textarea id="aditional" className='text-neutral-800' placeholder="Digite aqui" />
         </div>
+        <div className='mx-auto w-fit p-4'>
+        <button className='bg-white text-mainPink hover:scale-110 transition rounded-lg py-2 px-8 shadow-xl'>
+                Enviar
+                </button>
+        </div>
+      </div>
     </div>
   )
 }
